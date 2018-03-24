4eceba265d55e5af55b4e09367c07f7352fb9451
Christian Seidlitz <seidchr+github@gmail.com>
2018-03-24 15:30:38 +0100

    Update test cases in console

--- AutoTagger.TestConsole.Core/Program.cs
+++ AutoTagger.TestConsole.Core/Program.cs
@@ -7,8 +7,25 @@ namespace AutoTagger.TestConsole.Core
     {
         static void Main(string[] args)
         {
-            var db = new GraphDatabase();
-            var result = db.Submit("g.V()");
+            Console.WriteLine("G: Graph Database Test, C: Clarifai Tagger Test");
+            var key = Console.ReadKey();
+            switch (key.KeyChar)
+            {
+                case 'c':
+                case 'C':
+                    Console.WriteLine("Clarifai Test");
+                    break;
+                case 'd':
+                case 'D':
+                    Console.WriteLine("DB Test");
+                    var db = new GraphDatabase();
+                    var result = db.Submit("g.V()");
+                    break;
+                default:
+                    Console.WriteLine("No Test");
+                    break;
+            }
+
             Console.Read();
         }
     }
