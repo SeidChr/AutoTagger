19a53beb8e53eb78442f73d5f67db4ca31daac6a
Christian Seidlitz <seidchr+github@gmail.com>
2018-03-24 15:10:15 +0100

    Update Test Project

---
+++ AutoTagger.TestConsole.Core/Program.cs
@@ -0,0 +1,15 @@
+﻿using AutoTagger.Database.Standard;
+using System;
+
+namespace AutoTagger.TestConsole.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new GraphDatabase();
+            var result = db.Submit("g.V()");
+            Console.Read();
+        }
+    }
+}
