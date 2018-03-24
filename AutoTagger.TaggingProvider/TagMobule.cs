a300e25cf20cde8b4f5fb841b8021ea1c6984c1f
Dario D. Müller <dario-domi@hotmail.de>
2018-03-24 15:01:30 +0100

    added TaggingProvider GET /find with helloWorld

---
+++ AutoTagger.TaggingProvider/TagMobule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace AutoTagger.TaggingProvider
+{
+    public class TagMobule : NancyModule
+    {
+        public TagMobule()
+        {
+            Get["/find"] = parameters => "Hello World! :)";
+        }
+    }
+}
