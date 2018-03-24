3c4fb1f9b3c9ee160fac56d514395be35c53da15
Dario D. Müller <dario-domi@hotmail.de>
2018-03-24 22:12:36 +0100

    added userInterface image upload wip

--- AutoTagger.UserInterface/ImageController.cs
+++ AutoTagger.UserInterface/ImageController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using AutoTagger.Clarifai.Standard;
 using AutoTagger.Contract;
 using AutoTagger.Database.Standard;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +29,11 @@ namespace AutoTagger.UserInterface
         [HttpPost]
         public JsonResult Post([FromForm]string link)
         {
+            return DoTagging(link);
+        }
+
+        private JsonResult DoTagging(string link)
+        {
             var content = new Dictionary<string, object>();
             content.Add("link", link);
 
@@ -39,10 +47,27 @@ namespace AutoTagger.UserInterface
             }
             catch (NotImplementedException)
             {
-
             }
 
             return Json(content);
         }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<string> Upload(IFormFile formFile)
+        {
+            var filePath = Path.GetTempFileName();
+
+            if (formFile.Length > 0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+            }
+
+            return "YEAH";
+        }
+
     }
 }
