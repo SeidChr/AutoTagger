5d6d05a201bee34f8c77ae56a076bd3df31ac003
Dario D. Müller <dario-domi@hotmail.de>
2018-03-24 14:52:23 +0100

    added nancy rest handling

--- AutoTagger.TaggingProvider/Program.cs
+++ AutoTagger.TaggingProvider/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy.Hosting.Self;
 
 namespace AutoTagger.TaggingProvider
 {
@@ -10,6 +11,17 @@ namespace AutoTagger.TaggingProvider
     {
         static void Main(string[] args)
         {
+            var hostConfigs = new HostConfiguration();
+            hostConfigs.UrlReservations.CreateAutomatically = true;
+            var url = "http://localhost:80";
+            var uri = new Uri(url);
+            using (var host = new NancyHost(hostConfigs, uri))
+            {
+                host.Start();
+                Console.WriteLine("Running AutoTaggingProvider on: " + url);
+                Console.ReadLine();
+            }
+            Console.WriteLine("System shut down :'(");
 
         }
     }
