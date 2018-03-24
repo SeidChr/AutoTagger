dc71556635fb1c08544bac551e360a9cce63227c
Dario D. Müller <dario-domi@hotmail.de>
2018-03-24 21:45:45 +0100

    added userInterface as asp.net core with rest api

---
+++ AutoTagger.UserInterface/ImageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoTagger.Clarifai.Standard;
+using AutoTagger.Contract;
+using AutoTagger.Database.Standard;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutoTagger.UserInterface
+{
+    [Route("api/[controller]")]
+    public class ImageController : Controller
+    {
+
+        // POST api/<controller>
+        [HttpPost]
+        public JsonResult Post([FromForm]string link)
+        {
+            var content = new Dictionary<string, object>();
+            content.Add("link", link);
+
+            var _taggingProvider = new ClarifaiImageTagger() as ITaggingProvider;
+            var _db = new AutoTaggerDatabase() as IAutoTaggerDatabase;
+
+            var maschineTags = _taggingProvider.GetTagsForImage(link).ToList();
+            content.Add("maschineTags", maschineTags);
+
+            try
+            {
+                var instagramTags = _db.FindInstagramTags(maschineTags);
+                content.Add("instagramTags", instagramTags);
+            }
+            catch (NotImplementedException)
+            {
+
+            }
+
+            return Json(content);
+        }
+    }
+}
--- AutoTagger.UserInterface/Program.cs
+++ AutoTagger.UserInterface/Program.cs
@@ -1,41 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using AutoTagger.Clarifai.Standard;
-using AutoTagger.Contract;
-using AutoTagger.Database.Standard;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AutoTagger.UserInterface
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var tagger = new ClarifaiImageTagger() as ITaggingProvider;
-            Console.WriteLine("Hello, please insert an URL");
-
-            while (true)
-            {
-                var link = Console.ReadLine();
-                Console.WriteLine("inserted: '" + link + "'");
-
-                var maschineTags = tagger.GetTagsForImage(link).ToList();
-                Console.WriteLine("Contains maschineTags: " + String.Join(", ", maschineTags));
-
-                var db = new AutoTaggerDatabase() as IAutoTaggerDatabase;
-
-                try
-                {
-                    var instagramTags = db.FindInstagramTags(maschineTags).ToList();
-                    var output = JsonConvert.SerializeObject(instagramTags);
-                    Console.WriteLine(output);
-                }
-                catch (NotImplementedException e)
-                {
-
-                }
-
-            }
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
