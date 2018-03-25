7d0b5e7a381934056704249d0b6935a9cc63daf3
Christian Seidlitz <seidchr+github@gmail.com>
2018-03-25 23:02:01 +0200

    Add Crawler DB Interface

---
+++ AutoTagger.Contract/ICrawlerDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AutoTagger.Contract
+{
+    public interface ICrawlerDatabase
+    {
+        void InsertOrUpdate(ICrawlerImage crawlerImage);
+    }
+}
