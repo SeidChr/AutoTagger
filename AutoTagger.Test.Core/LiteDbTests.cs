26b08d7093fddc28736af890319d63d0c26b5277
Christian Seidlitz <seidchr+github@gmail.com>
2018-03-31 12:18:09 +0200

    litedb query test method to start with

--- AutoTagger.Test.Core/LiteDbTests.cs
+++ AutoTagger.Test.Core/LiteDbTests.cs
@@ -4,10 +4,44 @@ namespace AutoTagger.Test.Core
 
     using AutoTagger.Database.Standard;
 
+    using LiteDB;
+
     using Xunit;
 
     public class LiteDbTests
     {
+        [Fact]
+        public void LiteDbQueryTests()
+        {
+            var database       = new LiteDatabase("queryTest.ldb");
+            var testCollection = database.GetCollection("test");
+
+            testCollection.Upsert(new BsonDocument
+            {
+                ["_id"] = "1",
+                ["a"]   = this.Bson(1, 2, 3, 4),
+            });
+
+            testCollection.Upsert(new BsonDocument
+            {
+                ["_id"] = "2",
+                ["a"]   = this.Bson(3, 4, 5, 6)
+            });
+
+            // var expr  = new BsonExpression("SUM($.Items[*].Unity * $.Items[*].Price)");
+            // var total = expr.Execute(doc, true).First().AsDecimal;
+            var result = testCollection.Find(Query.LT("SUM($.a[*])", 11)).ToList();
+            Assert.Single(result);
+            Assert.Collection(
+                result,
+                resultDoc =>
+                {
+                    Assert.Equal(10, resultDoc["a"].AsArray.Aggregate(0, (i, v) => v.AsInt32 + i));
+                });
+
+            database.DropCollection("test");
+        }
+
         [Fact]
         public void LiteTaggingDbTest()
         {
@@ -25,6 +59,16 @@ namespace AutoTagger.Test.Core
             Assert.DoesNotContain("hX", tags);
 
             db.Drop();
+        }
+
+        private BsonValue Bson<T>(T x)
+        {
+            return new BsonValue(x);
+        }
+
+        private BsonArray Bson<T>(params T[] x)
+        {
+            return new BsonArray(x.Select(this.Bson));
         }
     }
 }
