447d696be29a65a40f9b646ef2c69c4d12e02330
Dario D. Müller <dario-domi@hotmail.de>
2018-04-28 14:40:56 +0200

    removed old stuff

--- AutoTagger.TestConsole.Core/Program.cs
+++ AutoTagger.TestConsole.Core/Program.cs
@@ -12,112 +12,115 @@
 
     internal class Program
     {
-        public static void ImportInstagramTags()
-        {
-            var linkWithTags = File.ReadLines("./imageLinks.txt").ToList();
-            foreach (var linkWithTag in linkWithTags)
-            {
-                var splitted     = linkWithTag.Split(',');
-                var link         = splitted.First();
-                var humanoidTags = splitted.Skip(1).First().Split('/');
+        //public static void ImportInstagramTags()
+        //{
+        //    var linkWithTags = File.ReadLines("./imageLinks.txt").ToList();
+        //    foreach (var linkWithTag in linkWithTags)
+        //    {
+        //        var splitted     = linkWithTag.Split(',');
+        //        var link         = splitted.First();
+        //        var humanoidTags = splitted.Skip(1).First().Split('/');
 
-                var tagger = new ClarifaiImageTagger();
-                var repository = new CosmosAutoTaggerStorage();
+        //        var tagger = new ClarifaiImageTagger();
+        //        var repository = new CosmosAutoTaggerStorage();
 
-                var machineTags = tagger.GetTagsForImageUrl(link);
+        //        var machineTags = tagger.GetTagsForImageUrl(link);
 
-                repository.InsertOrUpdate(link, machineTags, humanoidTags);
-            }
-        }
+        //        repository.InsertOrUpdate(link, machineTags, humanoidTags);
+        //    }
+        //}
 
-        private static void ClarifaiTest()
-        {
-            var imageLink =
-                "https://images.pexels.com/photos/211707/pexels-photo-211707.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+        //private static void ClarifaiTest()
+        //{
+        //    var imageLink =
+        //        "https://images.pexels.com/photos/211707/pexels-photo-211707.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
 
-            Console.WriteLine("Clarifai Test");
+        //    Console.WriteLine("Clarifai Test");
 
-            var tagger = new ClarifaiImageTagger();
-            var tags   = tagger.GetTagsForImageUrl(imageLink);
+        //    var tagger = new ClarifaiImageTagger();
+        //    var tags   = tagger.GetTagsForImageUrl(imageLink);
 
-            Console.WriteLine("Tags: " + string.Join(", ", tags));
-        }
+        //    Console.WriteLine("Tags: " + string.Join(", ", tags));
+        //}
 
-        private static void DatabaseReadTest()
-        {
-            var repository = new CosmosAutoTaggerStorage();
-            var instagramTags = repository.FindHumanoidTags(new List<string> { "boot", "fisch", "egal" });
+        //private static void DatabaseReadTest()
+        //{
+        //    var repository = new CosmosAutoTaggerStorage();
+        //    var instagramTags = repository.FindHumanoidTags(new List<string> { "boot", "fisch", "egal" });
 
-            Console.WriteLine("You should use the following instagram tags:");
-            foreach (var tag in instagramTags)
-            {
-                Console.WriteLine(tag);
-            }
-        }
+        //    Console.WriteLine("You should use the following instagram tags:");
+        //    foreach (var tag in instagramTags)
+        //    {
+        //        Console.WriteLine(tag);
+        //    }
+        //}
 
-        private static void DatabaseInsertTest()
-        {
-            // Console.WriteLine("Database Test");
-            // var db = new CosmosGraphDatabase();
-            // var result = db.Submit("g.V()");
-            var repository = new CosmosAutoTaggerStorage();
-            //context.Drop();
+        //private static void DatabaseInsertTest()
+        //{
+        //    // Console.WriteLine("Database Test");
+        //    // var db = new CosmosGraphDatabase();
+        //    // var result = db.Submit("g.V()");
+        //    var repository = new CosmosAutoTaggerStorage();
+        //    //context.Drop();
 
-            repository.InsertOrUpdate("schiff1", new[] { "boot", "wasser" }, new[] { "urlaub", "entspannung" });
+        //    repository.InsertOrUpdate("schiff1", new[] { "boot", "wasser" }, new[] { "urlaub", "entspannung" });
 
-            repository.InsertOrUpdate("boot1", new[] { "boot", "fisch" }, new[] { "urlaub", "angeln" });
+        //    repository.InsertOrUpdate("boot1", new[] { "boot", "fisch" }, new[] { "urlaub", "angeln" });
 
-            Console.WriteLine("Graph reset and filled.s");
-        }
+        //    Console.WriteLine("Graph reset and filled.s");
+        //}
 
-        private static IEnumerable<string> GetRandomInstagramTags(int seed)
-        {
-            var result = new List<string>();
-            var tags   = File.ReadLines("./instagramTags.txt").ToList();
-            var random = new Random(seed);
-            while (result.Count < 30)
-            {
-                var index = random.Next(tags.Count - 1);
-                var item  = tags[index];
-                tags.RemoveAt(index);
-                result.Add(item);
-            }
+        //private static IEnumerable<string> GetRandomInstagramTags(int seed)
+        //{
+        //    var result = new List<string>();
+        //    var tags   = File.ReadLines("./instagramTags.txt").ToList();
+        //    var random = new Random(seed);
+        //    while (result.Count < 30)
+        //    {
+        //        var index = random.Next(tags.Count - 1);
+        //        var item  = tags[index];
+        //        tags.RemoveAt(index);
+        //        result.Add(item);
+        //    }
 
-            return result;
-        }
+        //    return result;
+        //}
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("1: Database Insert \n" +
-                              "2: Database Read\n" +
-                              "3: Clarifai Tagger\n" +
-                              "4: Import Testdata from File\n" +
-                              "5: Start Crawler\n" +
-                              "6: Start ImageProcessor"
-                              );
+            Console.WriteLine("" + 
+                             //"1: Database Insert \n" +
+                             //"2: Database Read\n" +
+                             //"3: Clarifai Tagger\n" +
+                             //"4: Import Testdata from File\n" +
+                             "1: Start Crawler\n" +
+                             "2: Start ImageProcessor"
+                             );
             while(true)
             {
                 var key = Console.ReadKey();
                 Console.WriteLine("");
                 switch (key.KeyChar)
                 {
+                    //case '1':
+                    //    DatabaseInsertTest();
+                    //    break;
+
+                    //case '2':
+                    //    DatabaseReadTest();
+                    //    break;
+
+                    //case '3':
+                    //    ClarifaiTest();
+                    //    break;
+
                     case '1':
-                        DatabaseInsertTest();
-                        break;
-
-                    case '2':
-                        DatabaseReadTest();
-                        break;
-
-                    case '3':
-                        ClarifaiTest();
-                        break;
-
-                    case '5':
+                        Console.WriteLine("Start Crawler...");
                         StartCrawler();
                         break;
 
-                    case '6':
+                    case '2':
+                        Console.WriteLine("Start Image Processor...");
                         StartImageProcessor();
                         break;
                 }
@@ -157,10 +160,6 @@
             ImageProcessorApp.OnDbInserted += image =>
             {
                 Console.WriteLine("DB Insert for " + image.Id);
-            };
-            ImageProcessorApp.OnDbSleep += () =>
-            {
-                //Console.WriteLine("DB is sleeping for a while");
             };
             ImageProcessorApp.OnDbSaved += () =>
             {
